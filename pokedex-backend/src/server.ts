983c5e0f1ca611b62ea8d263a5964177b91bda85
panisara-bd <panisara.bnn@gmail.com>
2025-07-25 16:42:48 +0700

    Upgrade nodejs to 22 and upgrade dependencies

--- pokedex-backend/src/server.ts
+++ pokedex-backend/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import payload from 'payload';
 
 require('dotenv').config();
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 const app = express();
 
 const start = async () => {
